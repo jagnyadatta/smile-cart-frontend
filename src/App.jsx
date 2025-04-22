ee41cea9a080b58581d4722a89099bc41b1f9f7a
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-04-22 23:48:33 +0530

    Replaced CartItemsContext with useCartItemsStore

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import routes from "apis/routes";
 import { PageNotFound } from "components/commons";
 import Product from "components/Product/index";
@@ -7,31 +5,26 @@ import { Route, Switch, NavLink, Redirect } from "react-router-dom";
 
 import "./App.css";
 import ProductList from "./components/ProductList";
-import CartItemsContext from "./contexts/CartItemsContext";
 
-const App = () => {
-  const [cartItems, setCartItems] = useState([]);
-
-  return (
-    <CartItemsContext.Provider value={[cartItems, setCartItems]}>
-      <div className="mx-4 flex space-x-2">
-        <NavLink
-          exact
-          activeClassName="underline font-bold"
-          className="text-2xl"
-          to="/"
-        >
-          Home
-        </NavLink>
-      </div>
-      <Switch>
-        <Route exact component={Product} path={routes.products.show} />
-        <Route exact component={ProductList} path={routes.products.index} />
-        <Redirect exact from={routes.root} to={routes.products.index} />
-        <Route component={PageNotFound} path="*" />
-      </Switch>
-    </CartItemsContext.Provider>
-  );
-};
+const App = () => (
+  <>
+    <div className="mx-4 flex space-x-2">
+      <NavLink
+        exact
+        activeClassName="underline font-bold"
+        className="text-2xl"
+        to="/"
+      >
+        Home
+      </NavLink>
+    </div>
+    <Switch>
+      <Route exact component={Product} path={routes.products.show} />
+      <Route exact component={ProductList} path={routes.products.index} />
+      <Redirect exact from={routes.root} to={routes.products.index} />
+      <Route component={PageNotFound} path="*" />
+    </Switch>
+  </>
+);
 
 export default App;
