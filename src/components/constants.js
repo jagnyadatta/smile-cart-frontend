9c62c80362de31a84f23cbf1c9aeba46f1371498
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-04-26 23:44:12 +0530

    Implemented yup validation for checkout form

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,3 +1,6 @@
+import { t } from "i18next";
+import * as yup from "yup";
+
 export const IMAGE_URLS = [
   "https://ik.imagekit.io/d9mvewbju/SmileCart/thumbnail_61_7PaLfb.jpg",
   "https://ik.imagekit.io/d9mvewbju/SmileCart/1_yuRfcETI5.jpg",
@@ -10,3 +13,33 @@ export const VALID_COUNT_REGEX = /^(?:\d*|)$/;
 export const MRP = "mrp";
 
 export const OFFER_PRICE = "offerPrice";
+
+export const CHECKOUT_FORM_VALIDATION_SCHEMA = yup.object().shape({
+  email: yup
+    .string()
+    .email(t("validations.emailInvalid"))
+    .required(t("validations.emailRequired")),
+  country: yup
+    .object()
+    .shape({
+      name: yup.string().required(),
+      code: yup.string().required(),
+    })
+    .nullable(),
+  firstName: yup.string().required(t("validations.firstNameRequired")),
+  lastName: yup.string().required(t("validations.lastNameRequired")),
+  address: yup.string().required(t("validations.addressRequired")),
+  apartment: yup.string().required(t("validations.apartmentRequired")),
+  city: yup.string().required(t("validations.cityRequired")),
+
+  state: yup
+    .object()
+    .shape({
+      name: yup.string().required(),
+      code: yup.string().required(),
+    })
+    .nullable()
+    .required(t("validations.stateRequired")),
+
+  zipCode: yup.number().required(t("validations.zipCodeRequired")),
+});
