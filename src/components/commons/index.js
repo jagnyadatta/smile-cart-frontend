79e8a4eb833668859e30028a5e10a169e9152d79
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-04-27 14:12:14 +0530

    small changes done

--- src/components/commons/index.js
+++ src/components/commons/index.js
@@ -1,6 +1,17 @@
-import Header from "./Header";
-import PageLoader from "./PageLoader";
+import { Header, PageLoader } from "components/commons";
+
+import AddToCart from "./AddToCart";
+// import Header from "./Header";
+// import PageLoader from "./PageLoader";
 import PageNotFound from "./PageNotFound";
+import ProductQuantity from "./ProductQuantity";
 import TooltipWrapper from "./TooltipWrapper";
 
-export { Header, PageNotFound, PageLoader, TooltipWrapper };
+export {
+  AddToCart,
+  Header,
+  PageNotFound,
+  PageLoader,
+  ProductQuantity,
+  TooltipWrapper,
+};
